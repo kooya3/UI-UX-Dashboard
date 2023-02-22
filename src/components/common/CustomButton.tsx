d1900e7cc3b223ee2f6ebc129098c8ba584a2a70
kooya3 <elyeestatua@gmail.com>
2023-02-22 22:56:18 +0300

    created & styled the custom add property button

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,9 +1,28 @@
-import { Button } from '@pankod/refine-mui'
+import { Button, fontWeight, textTransform } from '@pankod/refine-mui'
 import { CustomButtonProps } from '../../interfaces/common';
 const CustomButton = ({ type, title, backgroundColor, color, fullWidth, icon, handleClick}: CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
+    <Button
+    sx={{
+      flex: fullWidth ? 1 : 'unset',
+      padding: '10px 15px',
+      width: fullWidth ? '100%' : 'fit-content',
+      minWidth: 130,
+      backgroundColor,
+      color,
+      fontSize: 16,
+      fontWeight: 600,
+      gap: '10px',
+      textTransform: 'capitalize', '&:hover': {
+        opacity: 0.9,
+        backgroundColor
+      }
+    }}
+    onClick={handleClick}
+    >
+        {icon}
+        {title}
+    </Button>
   )
 }
-
 export default CustomButton
