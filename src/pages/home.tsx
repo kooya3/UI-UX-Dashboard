b0c26f29570c7940b92770c25e195dfd16891326
kooya3 <elyeestatua@gmail.com>
2023-02-22 13:57:18 +0300

    created the piechart component and styled it

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,7 +10,30 @@ const home = () => {
       </Typography>
 
       <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-        <PieChart />
+        <PieChart 
+          title="Properties for Sale"
+          value={684}
+          series={[75,25]}
+          colors={['#475be8', '#e4e8ef']}
+         />  
+         <PieChart 
+         title="Properties for Rent"
+         value={5684}
+         series={[75,25]}
+         colors={['#475ae8', '#e4b8ef']}
+        />
+        <PieChart 
+          title="Total Customers"
+          value={684}
+          series={[75,25]}
+          colors={['#275be8', '#c4e8ef']}
+         />
+        <PieChart 
+          title="Properties for Cities"
+          value={555}
+          series={[75,25]}
+          colors={['#475be8', '#e4e8ef']}
+         />         
       </Box>
 
     </Box>
