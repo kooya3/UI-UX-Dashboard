a1df7480747dc1f59a0d309d023e315290b7bf44
kooya3 <elyeestatua@gmail.com>
2023-02-22 15:01:30 +0300

    editing the file + styling box element

--- src/components/charts/PropertyReferrals.tsx
+++ src/components/charts/PropertyReferrals.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Box, Stack, Typography } from '@pankod/refine-mui';
+import { propertyReferralsInfo } from '../../constants';
 
 const PropertyReferrals = () => {
   return (
-    <div>PropertyReferrals</div>
+    <Box
+    p={4}
+    bgcolor="#fcfcfc"
+    id="chart"
+    minWidth={490}
+    display="flex"
+    flexDirection="column"
+    borderRadius="15px"
+    >
+
+    </Box>
   )
 }
 
