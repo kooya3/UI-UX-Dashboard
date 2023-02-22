9239c4378a5db4a1b1aeed0fe7a06cd6e3499216
kooya3 <elyeestatua@gmail.com>
2023-02-22 11:56:50 +0300

    creating the dashboard section of the page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,19 @@
 import { useList } from "@pankod/refine-core";
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+import { PieChart, PropertyReferrals, TotalRevenue, PropertyCard, TopAgent  } from "../components";
 
 const home = () => {
   return (
-    <div>home</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color='#11142D'>
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart />
+      </Box>
+
+    </Box>
   )
 }
 
