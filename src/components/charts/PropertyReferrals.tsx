6cbe632644d0b1c1d32a7ff4128c7ea14a055f44
kooya3 <elyeestatua@gmail.com>
2023-02-22 15:10:31 +0300

    created the progressbar

--- src/components/charts/PropertyReferrals.tsx
+++ src/components/charts/PropertyReferrals.tsx
@@ -1,6 +1,26 @@
 import { Box, Stack, Typography } from '@pankod/refine-mui';
 import { propertyReferralsInfo } from '../../constants';
 
+interface progressBarProps {
+  title: string,
+  percentage: number,
+  color: string,
+}
+
+const progressBar = ({ title, percentage, color }: progressBarProps) => (
+  <Box width="100%">
+    <Stack direction="row"
+    alignItems="center"
+    justifyContent="space-between"
+    >
+      <Typography fontSize={16} fontWeight={500} color="#11142d">{title}</Typography>
+      <Typography fontSize={16} fontWeight={500} color="#11142d">{percentage}%</Typography>
+
+    </Stack>
+
+  </Box>
+)
+
 const PropertyReferrals = () => {
   return (
     <Box
@@ -12,7 +32,13 @@ const PropertyReferrals = () => {
     flexDirection="column"
     borderRadius="15px"
     >
+      <Typography fontSize={18} fontWeight={600} color="#11142d">
+        Property Referrals
+      </Typography>
 
+      <Stack my="20px" direction="column" gap={4}>
+
+      </Stack>
     </Box>
   )
 }
