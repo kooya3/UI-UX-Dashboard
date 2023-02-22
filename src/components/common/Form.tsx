be86ddc62e0be1dd7f759748f542480343676692
kooya3 <elyeestatua@gmail.com>
2023-02-23 01:49:31 +0300

    creating a dropdown list menuitem

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -27,6 +27,44 @@ const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, on
             {...register('title', { rquired: true })}
             />
           </FormControl>
+          <FormControl>
+            <FormHelperText sx={{ fontWeight: 500, margin: '10px 0', fontSize:16, color: '#11142d'}}>
+              Enter Desription
+            </FormHelperText>
+            <TextareaAutosize 
+            minRows={5}
+            required
+            placeholder='Write description'
+            color="info"
+            style={{ width: '100%' , background: 'transparent', fontSize: '16px', borderColor: 'rgba(0,0,0,0.23)', borderRadius: 6, padding: 10, color: '#919191'}}
+            {...register('description', { rquired: true })}
+            />
+          </FormControl>
+            
+          <Stack direction="row" gap={4}>
+            <FormControl sx={{ flex: 1}}>
+              <FormHelperText sx={{
+                fontWeight: 500,
+                margin: '10px 0',
+                fontSize:16,
+                color: '#11142d'
+              }}>
+                Select Property Type
+              </FormHelperText>
+              <Select
+              variant="outlined"
+              color="info"
+              displayEmpty
+              required
+              inputProps={{ 'aria-label': 'without label'}}
+              defaultValue="apartment"
+              {...register('PropertyType', { rquired: true })}
+              >
+                <MenuItem value="apartment">Apartment</MenuItem>
+                <MenuItem value="Multi">Multi</MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
         </form>
       </Box>
     </Box>
