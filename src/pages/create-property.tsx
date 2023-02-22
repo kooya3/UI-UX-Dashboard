c0a6adc5d73e12da784c547b888415f30116cc9c
kooya3 <elyeestatua@gmail.com>
2023-02-22 23:09:31 +0300

    created a powerful hook to store the create property functionality

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState } from  'react';
+import { useGetIdentity } from '@pankod/refine-core';
+import { FieldValues, useForm } from '@pankod/refine-react-hook-form';
+import { useNavigate } from '@pankod/refine-react-router-v6';
 
-const createProperty = () => {
+import Form from '../components/common/Form';
+
+const CreateProperty = () => {
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const [propertyImage, setPropertyImage] = useState({ name: '', url: '' })
+  const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
+
   return (
     <div>create-property</div>
   )
 }
 
-export default createProperty
+export default CreateProperty
