bf43ab7e77245092f217a6d303caec9dad7c25f6
kooya3 <elyeestatua@gmail.com>
2023-02-22 13:59:09 +0300

    changed the colors on the donut charts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,14 +14,14 @@ const home = () => {
           title="Properties for Sale"
           value={684}
           series={[75,25]}
-          colors={['#475be8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
          />  
          <PieChart 
          title="Properties for Rent"
          value={5684}
          series={[75,25]}
-         colors={['#475ae8', '#e4b8ef']}
-        />
+         colors={['#275be8', '#c4e8ef']}
+         />
         <PieChart 
           title="Total Customers"
           value={684}
@@ -32,7 +32,7 @@ const home = () => {
           title="Properties for Cities"
           value={555}
           series={[75,25]}
-          colors={['#475be8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
          />         
       </Box>
 
